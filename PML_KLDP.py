54ea4d0aa1498d841ab27c0c12a1315dff31962c
fauzifarhansyah <yantosumar67@gmail.com>
2021-10-31 20:08:12 +0700

    update file PML_KLDP.py

---
+++ PML_KLDP.py
@@ -0,0 +1,8 @@
+r = float(input("Masukan Jari-jari : "))
+
+phi = 3.14
+luas = phi*r*r
+keliling = 2*phi*r
+
+print ("Luas Lingkaran adalah       = ",luas)
+print ("Keliling Lingkaran adalah   = ",keliling)
